1f7fa8c88dab1200bb487979bf6ff91d37547886
raoul (Dr ) <rgrasman@uva.nl>
2015-06-16 19:32:29 +0000

    Inserted set.seed(123) and set.seed(20) in documentation examples for more predictable computing times. Changed set.seed(123) in summary.cusp example after noticing that the summary.cusp example still exceeds 5 seconds on http://win-builder.r-project.org/upload.aspx
    Also changed pcusp to return an array without redundant dimensions (i.e., inserted drop() around the return value).
    
    Changed the first sentence Description: field in DESCRIPTION file so that it does not start with the package name anymore (it used to read "Cusp-catastrophe modeling using Cobb's ...").
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cusp@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/pcusp.R
+++ pkg/R/pcusp.R
@@ -1,30 +1,30 @@
 `pcusp.old` <-
-function (y, alpha, beta) 
-Vectorize(function(x) integrate(dcusp, -Inf, x, alpha = alpha, 
+function (y, alpha, beta)
+Vectorize(function(x) integrate(dcusp, -Inf, x, alpha = alpha,
     beta = beta)$value)(y)
 
 `pcusp` <- # new pcusp the utilizes the c-code underlying cusp.nc because the older pcusp uses integrate from within R
-  function (y, alpha, beta, subdivisions = 100, rel.tol = .Machine$double.eps^0.25, 
-            abs.tol = rel.tol, stop.on.error = TRUE, aux = NULL, keep.order = TRUE) 
+  function (y, alpha, beta, subdivisions = 100, rel.tol = .Machine$double.eps^0.25,
+            abs.tol = rel.tol, stop.on.error = TRUE, aux = NULL, keep.order = TRUE)
   {
     x = y
     limit <- as.integer(subdivisions)
-    if (limit < 1 || (abs.tol <= 0 && rel.tol < max(50 * .Machine$double.eps, 
-                                                    5e-29))) 
+    if (limit < 1 || (abs.tol <= 0 && rel.tol < max(50 * .Machine$double.eps,
+                                                    5e-29)))
       stop("invalid parameter values")
-    
+
     # remember the order of alpha and beta
     idx = order(alpha, beta)
-    
+
     # First compute the normalizing constants
     lower <- -Inf
     upper <- Inf
     inf <- 2 # integrate over (-infinity, infinity)
     bound <- 0
-    wk <- .External("cuspnc", as.double(alpha), as.double(beta), 
-                    as.double(bound), as.integer(inf), as.double(abs.tol), 
+    wk <- .External("cuspnc", as.double(alpha), as.double(beta),
+                    as.double(bound), as.integer(inf), as.double(abs.tol),
                     as.double(rel.tol), limit = limit)
-    
+
     # Second compute the unnormalized probabilities
     lower <- -Inf
     upper <- Inf
@@ -32,8 +32,8 @@ Vectorize(function(x) integrate(dcusp, -Inf, x, alpha = alpha,
     pvals = value = abs.error = subdivs = ierr = matrix(NA, length(x), length(idx))
     for (i in 1:length(x)) {
       bound <- x[i]
-      wk2 <- .External("cuspnc", as.double(alpha), as.double(beta), 
-                       as.double(bound), as.integer(inf), as.double(abs.tol), 
+      wk2 <- .External("cuspnc", as.double(alpha), as.double(beta),
+                       as.double(bound), as.integer(inf), as.double(abs.tol),
                        as.double(rel.tol), limit = limit)
       pvals[i, ] = wk2$value / wk$value
       value[i, ] = wk2$value
@@ -41,5 +41,5 @@ Vectorize(function(x) integrate(dcusp, -Inf, x, alpha = alpha,
       subdivs[i, ] = wk2$subdivisions
       ierr[i, ] = wk2$ierr
     }
-    if (!keep.order) pvals[,seq_along(idx)[idx]] else pvals # somehow this is inverted... ???
+    if (!keep.order) drop(pvals[,seq_along(idx)[idx]]) else drop(pvals) # somehow this is inverted... ???
   }
